cd17a9e0f2b4d2ef5a7ae41d4ff7eca1b83cd725
Леонов Артём Олегович <leonov.2018@stud.nstu.ru>
2021-11-15 20:12:17 +0700

    nstu login

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import login
 
 driver = webdriver.Firefox(executable_path='geckodriver.exe')
-driver.get("https://www.google.com")
+driver.get("https://login.nstu.ru/ssoservice/XUI/#login/&goto=http%3A%2F%2Fciu.nstu.ru%2Fstudent_study%2F")
 
 time.sleep(2)
-elem = driver.find_element_by_name("q")
+elem = driver.find_element_by_name("callback_0")
 elem.clear()
-elem.send_keys("python")
+elem.send_keys(login.log)
+elem = driver.find_element_by_name("callback_1")
+elem.clear()
+elem.send_keys(login.passw)
+elem = driver.find_element_by_name("callback_2")
 elem.send_keys(Keys.RETURN)
+
 time.sleep(5)
 driver.close()
